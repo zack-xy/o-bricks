1e738ca01c8a5fed20c4e76383b6ce18042c0f56
zack zheng <zack_zhengxiyun@163.com>
2021-11-01 22:01:59 +0800

    no message

--- mockServer/server/index.js
+++ mockServer/server/index.js
@@ -4,7 +4,11 @@ const route = require('./route')
 var cors = require('koa2-cors')
 const chalk = require('chalk')
 const bodyParser = require('koa-bodyparser')
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  allowMethods: ['GET', 'POST', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
 app.use(bodyParser())
 app.use(route.routes(), route.allowedMethods())
 app.listen(3060, () => {
--- mockServer/server/route.js
+++ mockServer/server/route.js
@@ -2,16 +2,18 @@ const router = require('koa-router')()
 const fs = require('fs')
 const path = require('path')
 const NewMock = require('../utils')
+const PassThrough = require('stream').PassThrough
+
 
 // 项目：vue3-bms 
 const vue3Bms = require('../groups/vue3-bms')
 const Mock = new NewMock()
 vue3Bms.forEach(item => {
-    // get请求
-    router.get(item.path, async (ctx) => {
-      const res = await Mock.asyncGeneratedData({ ...item })
-       ctx.body = res
-    })
+  // get请求
+  router.get(item.path, async (ctx) => {
+    const res = await Mock.asyncGeneratedData({ ...item })
+    ctx.body = res
+  })
 })
 
 
@@ -19,11 +21,33 @@ vue3Bms.forEach(item => {
 // router.get('/(.*)', (ctx) => {
 //     ctx.body = '访问到服务器'
 // })
-// post请求
 
-router.post('/timeline/activities', (ctx) => {
-    ctx.body = JSON.parse(content)
+// 测试Server-Sent 
+// 不太会写服务端，这里有问题，连接关闭后无法再次连接
+router.get('/stream', (ctx) => {
+  var stream = new PassThrough()
+  var stream2 = new PassThrough()
+  const msgList = [
+    { message: "消息111111" }
+  ]
+  ctx.set({
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  interval = setInterval(function () {
+    msgList.unshift({
+      message: `消息：${new Date().getTime()}`,
+      userId: "zack"
+    })
+    stream2.write("data: " + JSON.stringify(msgList) + "\n\n")
+  }, 2000);
+  ctx.body = stream.pipe(stream2);
 })
 
 
+// post请求
+router.post('/test', (ctx) => {
+  ctx.body = {};
+})
 module.exports = router
