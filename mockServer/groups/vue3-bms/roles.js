cfbd5cc7ce00e21fdbf4685777c20f154f6558a9
zack zheng <zack_zhengxiyun@163.com>
2021-11-13 23:46:53 +0800

    no message

--- mockServer/groups/vue3-bms/roles.js
+++ mockServer/groups/vue3-bms/roles.js
@@ -197,15 +197,44 @@ module.exports = [
     }
   },
   {
-    path: `${baseApi}/roles/all/list`,   // 角色列表
+    path: `${baseApi}/roles/allList`,   // 角色列表
     format: {
-      code: 200,
-      'data|2-5': [
+      "code": 200,
+      "data": [
         {
-          '_id': '@id',
-          'roleName|1': ['产品经理', '研发', '测试', '前端开发', '后端开发']
+          "_id": "60150cb764de99631b2c3cd3",
+          "roleName": "产品经理"
+        },
+        {
+          "_id": "60180b07b1eaed6c45fbebdb",
+          "roleName": "研发"
+        },
+        {
+          "_id": "60180b59b1eaed6c45fbebdc",
+          "roleName": "测试"
+        },
+        {
+          "_id": "60180b5eb1eaed6c45fbebdd",
+          "roleName": "JAVA"
+        },
+        {
+          "_id": "60180b64b1eaed6c45fbebde",
+          "roleName": "运维"
+        },
+        {
+          "_id": "60180b69b1eaed6c45fbebdf",
+          "roleName": "运营"
+        },
+        {
+          "_id": "60180b76b1eaed6c45fbebe0",
+          "roleName": "市场"
+        },
+        {
+          "_id": "60180b80b1eaed6c45fbebe1",
+          "roleName": "管理层"
         }
-      ]
+      ],
+      "msg": ""
     }
   },
   {
