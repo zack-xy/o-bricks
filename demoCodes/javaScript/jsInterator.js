88698dd1a87c72c00133b80a8945c082e8f082cc
zack zheng <zack_zhengxiyun@163.com>
2022-02-02 22:27:45 +0800

    no message

---
+++ demoCodes/javaScript/jsInterator.js
@@ -0,0 +1,23 @@
+// 实现可遍历结构
+class Counter {
+  constructor(limit) {
+    this.limit = limit;
+  }
+
+  [Symbol.iterator] () {
+    let count = 1, limit = this.limit;
+    return {
+      next () {
+        if (count <= limit) {
+          return { done: false, value: count++ };
+        } else {
+          return { done: true, value: undefined };
+        }
+      }
+    };
+  }
+}
+let counter = new Counter(3);
+for (let i of counter) {
+  console.log(i);
+}
