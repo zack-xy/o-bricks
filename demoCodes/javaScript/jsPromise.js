b5150f42997b34a7e98bdcea5af47912dec5fc63
zack zheng <zack_zhengxiyun@163.com>
2022-02-16 23:32:07 +0800

    no message

--- demoCodes/javaScript/jsPromise.js
+++ demoCodes/javaScript/jsPromise.js
@@ -18,3 +18,62 @@ let p10 = p1.then(() => { throw 'baz' })
 // 返回错误值依然是resolved
 // Promise <resolved>: Error: qux
 let p11 = p1.then(() => Error('qux'))
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+// 任意多个函数作为处理程序合成一个连续传值的期约连锁
+function compose (...fns) {
+  return (x) => fns.reduce((promise, fn) => promise.then(fn), Promise.resolve(x))
+}
+
+/**
+ * compose使用
+ */
+function addTwo (x) { return x + 2 }
+function addThree (x) { return x + 3 }
+function addFive (x) { return x + 5 }
+
+let addTen = compose(addTwo, addThree, addFive)
+addTen(8).then(console.log);
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+// promise进度
+class TrackablePromise extends Promise {
+  constructor(executor) {
+    // 存储通知函数
+    const notifyHandlers = [];
+
+    super((resolve, reject) => {
+      return executor(resolve, reject, (status) => {
+        notifyHandlers.map((hanlder) => hanlder(status))
+      })
+    })
+
+    this.notifyHandlers = notifyHandlers
+  }
+
+  notify (notifyHandler) {
+    this.notifyHandlers.push(notifyHandler)
+    return this
+  }
+}
+
+// 实例化一个promise
+let p = new TrackablePromise((resolve, reject, notify) => {
+  function countdown (x) {
+    if (x > 0) {
+      notify(`${20 * x}% remaining`)
+      setTimeout(() => countdown(x - 1), 1000)
+    } else {
+      resolve()
+    }
+  }
+
+  countdown(5)
+})
+
+p.notify((x) => setTimeout(console.log, 0, 'progress:', x))
+p.then(() => setTimeout(console.log, 0, 'completed'))
