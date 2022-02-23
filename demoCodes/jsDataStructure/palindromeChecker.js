74fcf14da07b6ed0bb15caa11a3c85e873f3c9fc
zack zheng <zack_zhengxiyun@163.com>
2022-02-23 21:23:04 +0800

    no message

---
+++ demoCodes/jsDataStructure/palindromeChecker.js
@@ -0,0 +1,27 @@
+// 回文检查器
+function palindromeChecker (aString) {
+  if (aString === undefined || aString === null || (aString !== null && aString.length === 0)) {
+    return false
+  }
+  const deque = new Deque();
+  const lowerString = aString.toLocaleLowerCase().split(' ').join('') // 去除空格，转换为小写
+  let isEqual = true
+  let firstChar, lastChar
+
+
+  for (let i = 0; i < lowerString.length; i++) {
+    deque.addBack(lowerString.charAt(i))
+  }
+
+
+  while (deque.size() > 1 && isEqual) {
+    firstChar = deque.removeFront()
+    lastChar = deque.removeBack()
+    if (firstChar !== lastChar) {
+      isEqual = false
+    }
+  }
+
+
+  return isEqual
+}
