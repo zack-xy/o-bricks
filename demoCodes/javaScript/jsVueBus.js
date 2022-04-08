cded2d14e04afc07b5151bbca779f5c649dc9b2b
zack zheng <zack_zhengxiyun@163.com>
2022-04-08 14:06:08 +0800

    no message

---
+++ demoCodes/javaScript/jsVueBus.js
@@ -0,0 +1,19 @@
+// Bus: 事件派发、监听和回调管理
+class Bus {
+  constructor() {
+    this.callbacks = {}
+  }
+
+  $on (name, fn) {
+    this.callbacks[name] = this.callbacks[name] || []
+    this.callbacks[name].push(fn)
+  }
+  $emit (name, args) {
+    if (this.callbacks[name]) {
+      this.callbacks[name].forEach(cb => cb(args));
+    }
+  }
+}
+
+// main.js
+Vue.prototype.$bus = new Bus()
