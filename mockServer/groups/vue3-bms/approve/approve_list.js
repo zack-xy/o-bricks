01ad98eaf9ab0aecf71797406f9079281e24c412
zack_zheng <zack_zhengxiyun@163.com>
2022-06-08 22:23:04 +0800

    no message

--- mockServer/groups/vue3-bms/approve/approve_list.js
+++ mockServer/groups/vue3-bms/approve/approve_list.js
@@ -1,24 +1,24 @@
 module.exports = {
   code: 200,
   data: {
-    page: {
+    'page': {
       pageNum: 1,
       pageSize: 20,
-      total: 50
+      total: 50,
     },
     'list|5-30': [
       {
-        applyUser: {
+        'applyUser': {
           userId: '@id',
-          userName: '@first'
+          userName: '@first',
         },
         'applyState|1': [1, 2, 3, 4],
         '_id': '@id',
         'applyType|1': [1, 2, 3, 4],
-        startTime: '@datetime',
-        endTime: '@datetime',
-        'leaveTime': '2天'
-      }
-    ]
-  }
+        'startTime': '@datetime',
+        'endTime': '@datetime',
+        'leaveTime': '2天',
+      },
+    ],
+  },
 }
