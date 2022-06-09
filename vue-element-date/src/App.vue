c95f51e71a2783ee6c51daa620c2e3c865493736
zack_zheng <zack_zhengxiyun@163.com>
2022-06-09 22:46:53 +0800

    引入element和pnpm

--- vue-element-date/src/App.vue
+++ vue-element-date/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <monthsPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import monthsPage from './pages/monthsPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    monthsPage,
   },
 }
 </script>
---
+++ vue-element-date/src/pages/monthsPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>实现非连续多选月份</h1>
+    <div class="block">
+      <span class="demonstration">多个日期:</span>
+      <el-date-picker type="dates" v-model="dates" placeholder="选择一个或多个日期">
+      </el-date-picker>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'months-page',
+  data() {
+    return {
+      dates: [],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
