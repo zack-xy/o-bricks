d0945ada890dd34b0e6d376bd4e5a2fadffb59f5
zack_zheng <zack_zhengxiyun@163.com>
2022-07-14 21:58:34 +0800

    code formatter

--- demoCodes/javaScript/jsInterator.js
+++ demoCodes/javaScript/jsInterator.js
@@ -1,23 +1,24 @@
+/* eslint-disable no-console */
 // 实现可遍历结构
 class Counter {
   constructor(limit) {
-    this.limit = limit;
+    this.limit = limit
   }
 
-  [Symbol.iterator] () {
-    let count = 1, limit = this.limit;
+  [Symbol.iterator]() {
+    let count = 1; const limit = this.limit
     return {
-      next () {
-        if (count <= limit) {
-          return { done: false, value: count++ };
-        } else {
-          return { done: true, value: undefined };
-        }
-      }
-    };
+      next() {
+        if (count <= limit)
+          return { done: false, value: count++ }
+
+        else
+          return { done: true, value: undefined }
+      },
+    }
   }
 }
-let counter = new Counter(3);
-for (let i of counter) {
-  console.log(i);
-}
+const counter = new Counter(3)
+for (const i of counter)
+  console.log(i)
+
--- demoCodes/javaScript/jsPipeStream.js
+++ demoCodes/javaScript/jsPipeStream.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-unexpected-multiline */
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 // 每1000毫秒生成一个递增的整数
-async function* ints () {
+async function* ints() {
   for (let i = 0; i < 5; ++i) {
     yield await new Promise((resolve) => {
       setTimeout(resolve, 1000, i)
@@ -7,40 +10,36 @@ async function* ints () {
   }
 }
 
-
 const integerStream = new ReadableStream({
-  async start (controller) {
-    for await (let chunk of ints()) {
+  async start(controller) {
+    for await (const chunk of ints())
       controller.enqueue(chunk)
-    }
+
     controller.close()
-  }
+  },
 })
 
-
 const doublingStream = new TransformStream({
-  transform (chunk, controller) {
+  transform(chunk, controller) {
     controller.enqueue(chunk * 2)
-  }
+  },
 })
 
 // 通过管道连接流
-const pipedStream = integerStream.pipeThrough(doublingStream);
+const pipedStream = integerStream.pipeThrough(doublingStream)
 
 // 从连接流的输出获得读取器
 const pipedStreamDefaultReader = pipedStream.getReader()
 
+// 消费者
+(async () => {
+  while (true) {
+    const { done, value } = await pipedStreamDefaultReader.read()
 
-  // 消费者
-  (async function () {
-    while (true) {
-      const { done, value } = await pipedStreamDefaultReader.read();
+    if (done)
+      break
 
-
-      if (done) {
-        break;
-      } else {
-        console.log(value)
-      }
-    }
-  })()
+    else
+      console.log(value)
+  }
+})()
