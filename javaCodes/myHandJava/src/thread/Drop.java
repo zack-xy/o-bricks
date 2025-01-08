bf3155d2c0be03c997919df3db554155360c621a
zack zheng <zack_zhengxiyun@163.com>
2025-01-08 17:17:25 +0800

    1

--- javaCodes/myHandJava/src/thread/Drop.java
+++ javaCodes/myHandJava/src/thread/Drop.java
@@ -2,6 +2,8 @@ package thread;
 
 public class Drop {
   private String food;
+  // empty为true代表：消费者等待生产者生产
+  // empty为false代表：代表商品生产完毕，生产者还要等待消费者消费完
   private boolean empty = true;
   
   /**
@@ -9,9 +11,11 @@ public class Drop {
    * @return
    */
   public synchronized String get() {
+    // 消费者线程拿到drop对象的内部锁
     while(empty) {
       try {
-        wait();
+        // 调用wait，消费者线程会释放drop对象的内部锁，然后进入WAITING状态
+        wait();  
       } catch (InterruptedException e) {}
     }
     empty = true;
@@ -28,9 +32,9 @@ public class Drop {
       try {
         wait();
       } catch (InterruptedException e) {}
-      empty = false;
-      this.food = food;
-      notifyAll();
     }
+    empty = false;
+    this.food = food;
+    notifyAll();
   }
 }
