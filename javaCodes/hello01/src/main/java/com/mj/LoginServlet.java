0d573a4da3f4cefec79ebce0dd97fdde064eea65
zack zheng <zack_zhengxiyun@163.com>
2025-01-17 14:47:14 +0800

    1

--- javaCodes/hello01/src/main/java/com/mj/LoginServlet.java
+++ javaCodes/hello01/src/main/java/com/mj/LoginServlet.java
@@ -20,18 +20,7 @@ public class LoginServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 1、取出客户端发送的数据
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-        resp.setContentType("text/plain;charset=utf-8");
-        PrintWriter out = resp.getWriter();
-        // 2、判断用户名密码是否正确
-        if("123".equals(username) && "123".equals(password)) {
-            // 3、给客户端发送响应
-            out.write("登陆成功");
-        } else {
-            out.write("登陆失败");
-        }
+        doPost(req, resp);
     }
 
     /***
@@ -43,6 +32,49 @@ public class LoginServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        outHtml(req, resp);
+    }
+
+    /**
+     * HTTP请求
+     * > GET请求
+     *      1> 请求参数拼接在请求路径后面
+     *      2> 请求头
+     * > POST请求
+     *      1> 请求参数放在请求体中
+     *      2> 请求头
+     */
+
+    private void outHtml(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1. 设置请求数据的编码
+        req.setCharacterEncoding("UTF-8");
+
+        // 2. 获取请求参数
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        // 3. 先设置响应的内容类型(MIMEType + 数据编码)
+        resp.setContentType("text/html;charset=UTF-8");
+        // 4. 拿到输出流
+        PrintWriter out = resp.getWriter();
+
+        // 5. 判断
+        if("123".equals(username) && "123".equals(password)) {
+            out.write("<h1 style=\"color: blue; border: 1px solid black;\">登陆成功</h1>");
+            out.write("<ul>");
+            out.write("<li>个人信息</li>");
+            out.write("<li>修改密码</li>");
+            out.write("<li>退出登陆</li>");
+            out.write("</ul>");
+        } else {
+            out.write("<h1 style=\"color: red; border: 1px solid black;\">登陆失败</h1>");
+            out.write("<ul>");
+            out.write("<li>重新登陆</li>");
+            out.write("</ul>");
+        }
+    }
+
+    private void outPlain(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 0、设置请求数据的编码
         req.setCharacterEncoding("utf-8");
         // 1、取出客户端发送的数据
@@ -59,14 +91,4 @@ public class LoginServlet extends HttpServlet {
             out.write("登陆失败");
         }
     }
-
-    /**
-     * HTTP请求
-     * > GET请求
-     *      1> 请求参数拼接在请求路径后面
-     *      2> 请求头
-     * > POST请求
-     *      1> 请求参数放在请求体中
-     *      2> 请求头
-     */
 }
