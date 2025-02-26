e39fdbeb85771cf8e8386331eac2f1ca55261a03
zack zheng <zack_zhengxiyun@163.com>
2025-02-26 16:44:51 +0800

    1

--- demoCodes/react-study/react-demo/src/App.js
+++ demoCodes/react-study/react-demo/src/App.js
@@ -1,10 +1,13 @@
+// eslint-disable-next-line
 import UseStateCom from './useStateDemo';
+import UseEffectCom from './useEffectDemo';
 
 
 function App() {
   return (
     <div className="App">
-      <UseStateCom />
+      {/* <UseStateCom /> */}
+      <UseEffectCom/>
     </div>
   );
 }
