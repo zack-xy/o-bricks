0445134dfcacdb52f7c379f24d6c1bc24e46e5c0
zack zheng <zack_zhengxiyun@163.com>
2025-03-03 13:58:27 +0800

    1

---
+++ demoCodes/RustCodes/world_hello/src/hello_world.rs
@@ -0,0 +1,9 @@
+pub fn greet_world() {
+  let southern_germany = "Grüß Gott!";
+  let chinese = "世界，你好";
+  let english = "World, hello";
+  let regions = [southern_germany, chinese, english];
+  for region in regions.iter() {
+    println!("{}", &region)
+  }
+}
--- demoCodes/RustCodes/world_hello/src/main.rs
+++ demoCodes/RustCodes/world_hello/src/main.rs
@@ -1,36 +1,13 @@
+mod hello_world;
+mod module_demo;
+
+use hello_world::*;
+use module_demo::main as module_main;
+
 fn main() {
-    struct User {
-        active: bool,
-        username: String,
-        email: String,
-        sign_in_count: u64,
-    }
-
-    let mut user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
-    };
-    user1.email = String::from("anotheremail@example.com");
+   greet_world();
+   // module的导出的main函数命名重复
+   module_main();
 }
 
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
-}
-
-fn useless<'a>(first: &'a i32, second: &'a i32) {
-    
-}
