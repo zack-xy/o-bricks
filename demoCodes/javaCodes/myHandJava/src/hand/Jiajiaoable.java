7361b5f4671382166a42cbd2c7be09d2d33a0b32
zack zheng <zack_zhengxiyun@163.com>
2025-06-06 14:18:27 +0800

    1

--- demoCodes/javaCodes/myHandJava/src/hand/Jiajiaoable.java
+++
@@ -1,9 +0,0 @@
-package hand;
-
-public interface Jiajiaoable {
-  void jiaoZuQiu();
-  // 默认方法，可以定义实现
-  default void jiaoLanQiu() {
-    System.out.println("教篮球");
-  }
-}
--- demoCodes/javaCodes/myHandJava/src/hand/Test.java
+++
@@ -1,6 +0,0 @@
-package hand;
-
-public interface Test {
-  public abstract void test1();
-  public abstract void test2();
-}
