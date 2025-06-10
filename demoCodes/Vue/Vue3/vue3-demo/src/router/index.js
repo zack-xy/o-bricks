3de310e64dc8397b6ea7fc627ee8f4bdadaa7b6d
zack zheng <zack_zhengxiyun@163.com>
2025-06-10 15:47:34 +0800

    1

--- demoCodes/Vue/Vue3/vue3-demo/src/router/index.js
+++ demoCodes/Vue/Vue3/vue3-demo/src/router/index.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', component: () => import('@/pages/Home.vue') },
+    { path: '/signatureBoard', component: () => import('@/pages/SignatureBoard.vue')  },
   ]
 })
 
