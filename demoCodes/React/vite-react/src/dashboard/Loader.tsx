6fb659a62e9adecc631ceda3041de65ef76f7160
zack zheng <zack_zhengxiyun@163.com>
2025-08-05 10:31:16 +0800

    1

---
+++ demoCodes/React/vite-react/src/dashboard/Loader.tsx
@@ -0,0 +1,14 @@
+import { useLoaderData } from 'react-router'
+
+export default function LoaderPage() {
+  const data = useLoaderData()
+  return (
+    <div>
+      <h4>测试loader参数</h4>
+      <p>
+        数据：
+        {data.message}
+      </p>
+    </div>
+  )
+}
--- demoCodes/React/vite-react/src/main.tsx
+++ demoCodes/React/vite-react/src/main.tsx
@@ -1,37 +1,103 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router'
 import About from './About.tsx'
 import App from './App.tsx'
 import Home from './dashboard/Home.tsx'
 import Layout from './dashboard/Layout.tsx'
 import Link from './dashboard/Link.tsx'
+import Loader from './dashboard/Loader.tsx'
 import Setting from './dashboard/Setting.tsx'
 import Team from './dashboard/Team.tsx'
 import File from './File.tsx'
 
 import './index.css'
 
+// Declarative mode
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="about" element={<About />} />
+//         {/* 标*就是匹配所有路径 */}
+//         <Route path="files/*" element={<File />} />
+
+//         {/* 嵌套路由 */}
+//         <Route path="dashboard" element={<Layout />}>
+//           {/* 加index的是默认路由 */}
+//           <Route index element={<Home />} />
+//           <Route path="settings" element={<Setting />} />
+//           {/* 动态路由，这里有2段动态 */}
+//           <Route path="teams/:teamId/p/:productId" element={<Team />} />
+//           <Route path="link" element={<Link />} />
+//         </Route>
+
+//       </Routes>
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+
+// Data mode
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: App,
+  },
+  {
+    path: 'about',
+    element: <About />,
+  },
+  {
+    path: 'files/*',
+    element: <File />,
+  },
+  {
+    path: 'dashboard',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: 'settings',
+        Component: Setting,
+      },
+      {
+        path: 'link',
+        Component: Link,
+      },
+      {
+        path: 'teams/:teamId/p/:productId',
+        Component: Team,
+        loader: async ({ params }) => {
+          // eslint-disable-next-line no-console
+          console.log('params>>>>', params)
+          return { name: 'zack zheng' }
+        },
+      },
+    ],
+  },
+  {
+    path: 'projects',
+    children: [
+      { index: true, Component: Home },
+      {
+        Component: Layout,
+        children: [
+          { path: 'link', Component: Link },
+          { path: 'loader/:name', Component: Loader, loader: async ({ params }) => {
+            return { message: `hello ${params.name}` }
+          } },
+        ],
+      },
+    ],
+  },
+])
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="about" element={<About />} />
-        {/* 标*就是匹配所有路径 */}
-        <Route path="files/*" element={<File />} />
-
-        {/* 嵌套路由 */}
-        <Route path="dashboard" element={<Layout />}>
-          {/* 加index的是默认路由 */}
-          <Route index element={<Home />} />
-          <Route path="settings" element={<Setting />} />
-          {/* 动态路由，这里有2段动态 */}
-          <Route path="teams/:teamId/p/:productId" element={<Team />} />
-          <Route path="link" element={<Link />} />
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
