32360baa0ea3ac1d905956aaadb19f1c60d3a9cf
zack zheng <zack_zhengxiyun@163.com>
2025-09-27 18:57:10 +0800

    1

---
+++ demoCodes/cliDemos/zack-test/bin/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+// 注册命令 zack-test init
+const argv = require('process').argv
+
+const command = argv[2]
+
+// 参数解析 --version 和 init --name
+
+// console.log('Hello zack test!!!')
