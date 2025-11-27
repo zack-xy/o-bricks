adf6d2a3a21e175b988b172da934d7f025ce99d4
zack zheng <zack_zhengxiyun@163.com>
2025-11-27 18:22:05 +0800

    1

--- demoCodes/minigrep/src/lib.rs
+++ demoCodes/minigrep/src/lib.rs
@@ -9,12 +9,19 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn build(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-        return Err("参数不足");
-    }
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+  pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+    // 第一个参数是程序名，无需使用，直接空调用一次
+    args.next();
+
+    let query = match args.next() {
+      Some(arg) => arg,
+      None => return Err("没有获取到查询字符串")
+    };
+
+    let file_path = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a file path")
+    };
 
     let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -42,14 +49,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
-  let mut results = Vec::new();
+  // let mut results = Vec::new();
 
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-  results
+  // for line in contents.lines() {
+  //   if line.contains(query) {
+  //     results.push(line);
+  //   }
+  // }
+  // results
+
+  contents
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 pub fn search_case_insensitive<'a>(
--- demoCodes/minigrep/src/main.rs
+++ demoCodes/minigrep/src/main.rs
@@ -4,9 +4,8 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("参数解析错误: {err}");
         process::exit(1);
     });
